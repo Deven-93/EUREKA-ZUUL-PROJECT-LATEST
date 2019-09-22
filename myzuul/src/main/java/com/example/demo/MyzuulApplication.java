26049a5c27d62d20d06917be45d614881ed8fcda
Deven <AKASH@DESKTOP-3L1G32R>
2019-09-22 21:17:46 +0530

    New Repo

---
+++ myzuul/src/main/java/com/example/demo/MyzuulApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class MyzuulApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MyzuulApplication.class, args);
+		
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource cors = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("HEAD");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("POST");
+	    cors.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(cors);
+	}
+
+}
